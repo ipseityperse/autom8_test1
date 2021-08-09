2f4d9be3a85a2d36f0db251661634c63d532364f
ipseityperse <github@konopka.me>
2021-08-09 14:59:43 +0200

    fix?

--- report/scanner.py
+++ report/scanner.py
@@ -1,40 +1,175 @@
+from .settings import SCANNER_DEFAULTS as conf
 from .settings import log
-import fitz, os, uuid
+from PIL import Image
+import fitz, os
 
-def pdf2img(pdf_path:str, image_path:str, page_no:int) -> str:
-    log.debug(f"pdf2img(pdf_path:{pdf_path}, image_path:{image_path}, page:{page_no})")
-    pdf = fitz.open(pdf_path) # loads pdf as a fitz object
+class Scanner:
 
-    # Trying to get the page, checking for wrong page number
-    try:
-        page_no = int(page_no) - 1
-        page = pdf[page_no]
-        log.info(f'pdf2img: Page {page_no} loaded')
-    except ValueError:
-        raise ValueError(f'pdf2img: {type(page_no)} can not be a page number: {page_no}')
-    except:
-        raise ValueError(f'pdf2img: Page not found: {page_no}, page count: {pdf.pageCount}')
+    class PDFNotProvidedException(Exception):
+        pass
+
+
+    class PDFNotFoundException(Exception):
+        pass
+
     
-    # Standarize output, no rotation, resolution and no alpha layer
-    zoom = 1.33333333 # (1.33333333-->1056x816)   (2-->1584x1224)
-    matrix = fitz.Matrix(zoom, zoom).preRotate(0)
-    pixels = page.getPixmap(matrix=matrix, alpha=False)
+    working_pdf = False
+    working_page = False
+    working_pixels = False
+    working_image = False
 
-    # Create folder if does not exist
-    if not os.path.exists(image_path):
-        os.makedirs(image_path)
-        log.debug(f'pdf2img: image_path does not exist, creating folder {image_path}')
+
+    def __init__(self, path:str=None, page:int=None, crop:tuple=None, template:str=None, save=False) -> None:
+        log.debug(f"Scanner: on")
+        # Preload document if given the path
+        if path:
+            log.debug(f"Load document, path: {path}")
+            self.scan(path)
+        else:
+            log.debug(f"No path provided, skipping init")
+            return None
+        # Preload scan template data
+        if template:
+            log.debug(f"Loading scan template: {template}")
+            from .settings import PDF2IMG_EXPORT_PRESETS as preset
+            if template in preset:
+                page_temp = preset[template]['page']
+                crop_temp = preset[template]['crop']
+                log.info(f"Scan template: {preset[template]}")
+            else:
+                log.warning(f"Provided template does not exist: {template}")
+        # Skip template if custom page/crop
+        if not page and page_temp:
+            page = page_temp
+            if not crop: 
+                crop = crop_temp
+        # Process page image
+        if page:
+            self.page(page)
+            if crop:
+                self.crop(crop)
+        else:
+            return None
+        # Autosave on demand:
+        if save:
+            self.save()
+
+
+    def scan(self, pdf_path:str):
+        log.debug(f"SCAN: Document path: {pdf_path}")
+        # Try to load document from path, return it or raise exception when failed
+        try:
+            self.working_pdf = fitz.open(pdf_path)
+            return self.working_pdf
+        except:
+            raise self.PDFNotFoundException(f'Scanner:scan: Could not open provided document: {pdf_path}')
     
-    # Save temp image
-    temp_name = uuid.uuid4()
-    temp_path = f"{image_path}/{temp_name}.png"
-    pixels.writePNG(temp_path)
-    log.debug(f'pdf2img: Temp PNG saved: {temp_path}')
 
-    return temp_path
+    @property
+    def matrix(self, zoom_x=conf['zoom']['x'], zoom_y=conf['zoom']['y'], rotate=conf['rotate']):
+        # Can be used as property with default values or as a function with custom values
+        # Returns a matrix to be filled with pixels
+        log.debug(f"Creating matrix: zoom_x:{zoom_x}, zoom_y:{zoom_y}, rotate:{rotate}")
+        return fitz.Matrix(zoom_x, zoom_y).preRotate(rotate)
 
 
+    def page(self, number:int, pdf=None):
+        log.debug(f"Getting page {number}, pdf={pdf}")
+        if not pdf: # If pdf is not passed, try to use last parsed pdf
+            if self.working_pdf:
+                pdf = self.working_pdf
+                log.debug(f"PDF not provided, using last scanned document")
+            else:
+                log.error(f"PDF not provided, nor scanned, can not get page")
+                raise self.PDFNotfoundException("Scanner:page: Provide scan to get a page")
+        # Validate page number and retrive it
+        try:
+            # Shift to allign with array indexing (pages start from 1, index from 0)
+            number = int(number) - 1
+            page = pdf[number]
+            log.info(f'Scanner:page: Page {number+1} loaded')
+        except ValueError:
+            raise ValueError(f'Scanner:page: {type(number)} can not be a page number: {number}')
+        except KeyError:
+            raise KeyError(f'Scanner:page: Page[{number+1}] not found, page count: {pdf.pageCount}')
+        # Set current working page and return it
+        self.working_page = page
+        return self.working_page
 
-        
-def test():
-    pdf2img("./RRTest/ex/AAES/AAES - Top 10 Assets by Vurlnerabilities.PNG", "./", 1)
+
+    @property
+    def pixelmap(self, page=None, matrix=None, colorspace=conf['colorspace']):
+        # Can be used as a property given a working page being selected before
+        # Or as a function where the page and custom matrix can be passed
+        log.debug(f"Converting page to pixels, page={page}, matrix={matrix}")
+        if not page: # If page is not passed, try to use last parsed page
+            if self.working_page:
+                page = self.working_page
+                log.debug(f"Page not provided, using last selected page")
+            else:
+                log.error(f"Page not provided, nor selected before")
+                raise self.PDFNotfoundException("Scanner:page: Provide page to get a pixelmap")
+        if not matrix:
+            log.debug(f"Matrix not provided, using default values")
+            matrix = self.matrix
+        # Fill matrix with pixels from page, save state and return
+        self.working_pixels = page.getPixmap(matrix=matrix, alpha=False, colorspace=colorspace)
+        return self.working_pixels
+
+
+    @property
+    def image(self, pixels=None, colorspace=conf['colorspace']):
+        if not pixels:
+            if self.working_pixels:
+                pixels = self.working_pixels
+                log.debug(f"PixMap not provided, using last used pixels")
+            elif self.working_page:
+                pixels = self.pixelmap
+                log.debug(f"PixMap not provided, generating from last used page")
+            else:
+                log.error(f"Page not provided, nor selected before")
+                raise self.PDFNotfoundException("Scanner:page: Provide page to get a pixelmap")
+        try:
+            img = Image.frombytes(colorspace, [pixels.width, pixels.height], pixels.samples)
+        except:
+            log.error(f"Could not convert to PIL Image")
+            raise Exception("Could not convert to PIL Image")
+        self.working_image = img
+        return self.working_image
+
+
+    def crop(self, points:tuple, image=None):
+        if not image:
+            if self.working_image:
+                image = self.working_image
+                log.info("Image not provided, loading working image")
+            else:
+                image = self.image
+        self.working_image = image.crop(points)
+        return self.working_image
+
+
+    def image_path(self, path, filename, format):
+        if not os.path.exists(path):
+            os.makedirs(path)
+            log.debug(f'Scanner:image_path: Output image path does not exist, creating folder {path}')
+        if callable(filename):
+            filename = filename()
+        try:
+            filename = str(filename)
+            return f'{path}/{filename}.{format.lower()}'
+        except:
+            raise ValueError('Scanner:image_path: filename is not a valid string and can not be converted to one.')
+
+
+    def save(self, image=None, path:str=conf['output_image_path'],
+        filename=conf['output_image_filename'], format:str=conf['output_image_format']) -> str:
+        if not image:
+            if self.working_image:
+                image = self.working_image
+                log.info('Image not provided, using last working image')
+            else:
+                image = self.image
+        path = self.image_path(path, filename, format)
+        image.save(path, format)
+        return path
--- report/settings.py
+++ report/settings.py
@@ -1,11 +1,23 @@
 import logging as log
+import uuid
 
 log.basicConfig(filename='report.log', encoding='utf-8', level=log.DEBUG)
+
+SCANNER_DEFAULTS = {
+    'zoom': {
+        'x': 3,
+        'y': 3
+    },
+    'rotate': 0,
+    'output_image_format': 'png',
+    'output_image_filename': uuid.uuid4,
+    'output_image_path': './scan',
+    'colorspace': 'RGB'
+}
 
 PDF2IMG_EXPORT_PRESETS = {
     'AssestByVulnerabilities': {
         'page': 1,
-        'start': (0,0),
-        'end': (100,100)
+        'crop': (108,252,1677,621)
     }
 }
