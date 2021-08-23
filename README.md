b4c03510253e2af6fe0ceacfa8caf814d745259d
ipseityperse <github@konopka.me>
2021-08-23 15:56:03 +0200

    package: - added readme - added clasifiers

---
+++ README.md
@@ -0,0 +1,24 @@
+# Autom8
+`autom8` has ambition to be a tool for automating mundane work in GSSC.
+
+For now it only contains `report` module which can be used to:
+ - Generate reports in Rapid
+ - Download generated reports
+ - Select and convert parts of PDFs to images
+ - Do it all automatically
+
+## Installation
+
+Download `.whl` file from `dist` directory and install using [pip](https://pip.pypa.io/en/stable/):
+
+```bash
+pip install autom8-x.y-py3-none-any.whl
+```
+
+## Usage
+
+```bash
+py -m autom8
+```
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- setup.py
+++ setup.py
@@ -5,11 +5,20 @@ setuptools.setup(
     author="Daniel Konopka",
     author_email="daniel@konopka.me",
     description="Package to autom8 mundane work",
+    license='MIT',
+    download_url="https://github.com/ipseityperse/autom8",
     packages=setuptools.find_packages(),
-    python_requires='>=3.9',
+    python_requires='>=3.8',
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Utilities"
         "Operating System :: OS Independent",
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+
     ],
     install_requires = [
         'certifi==2021.5.30',
